eae7138f695c7f91082ec2268e4dff5ec6e15b2a
chw90 <72135724+chw90@users.noreply.github.com>
2023-09-21 16:46:42 +0200

    Throw error in case of particle loss for wall boundaries

--- src/boundaries.h
+++ src/boundaries.h
@@ -2,6 +2,7 @@
 #define BOUNDARIES_H_
 
 #include "types.h"
+#include <stdexcept>
 
 template<int dim>
 class Boundary {
@@ -20,17 +21,21 @@ class BoundaryNone : public Boundary<dim> {
 template<int dim>
 class BoundaryWallHarmonic : public Boundary<dim> {
   // fixed walls with harmonic repulsive potential
-  double const epsilon;
-  double const cutoff;
+  double const epsilon;         // scaling factor
+  double const cutoff;          // cutoff in percent of the box edge length
   public:
     BoundaryWallHarmonic(double epsilon, double cutoff) : epsilon(epsilon), cutoff(cutoff) {};
     void apply(Particle<dim> &p, Box<dim> b) {
       // set repulsive force
       for ( int k = 0; k < dim; k++ ) {
+        auto l = b.hi[k]-b.lo[k];    // box length in dimension k
         auto dlo = p.x[k] - b.lo[k]; // distance to lower box bound
         auto dhi = b.hi[k] - p.x[k]; // distance to upper box bound
-        if ( dlo < cutoff ) p.f[k] += - 2.0*epsilon*(dlo-cutoff);
-        if ( dhi < cutoff ) p.f[k] -= - 2.0*epsilon*(dhi-cutoff);
+        if ( dlo < 0.0 || dhi < 0.0 ) {
+          throw std::runtime_error("BoundaryWallHarmonic: A particle has left the box.");
+        }
+        if ( dlo < cutoff*l ) p.f[k] += - 2.0*epsilon*(dlo-cutoff*l);
+        if ( dhi < cutoff*l ) p.f[k] -= - 2.0*epsilon*(dhi-cutoff*l);
       }
     }
 };
