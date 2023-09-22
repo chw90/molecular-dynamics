f7b0e97b437d3c1c6fb6ae3e33039e3947a6212f
chw90 <72135724+chw90@users.noreply.github.com>
2023-09-22 14:31:26 +0200

    Fix indentation

--- src/types.h
+++ src/types.h
@@ -12,12 +12,12 @@ template<int dim=DIM>
 class Particle {
   // stores particle data
   public:
-    int type;                     // type ID
-    double m;                     // mass
-    array<dim> x;                      // position
-    array<dim> v;                      // velocity
-    array<dim> f;                      // force
-    array<dim> buffer;                 // force buffer
+    int type;                   // type ID
+    double m;                   // mass
+    array<dim> x;               // position
+    array<dim> v;               // velocity
+    array<dim> f;               // force
+    array<dim> buffer;          // force buffer
     Particle(int t, double m, array<dim> x, array<dim> v, array<dim> f, array<dim> buffer) : type(t), m(m), x(x), v(v), f(f), buffer(buffer) {};
     Particle(int t, double m) : type(t), m(m) {
       x.fill(0.0); v.fill(0.0); f.fill(0.0); buffer.fill(0.0);
@@ -28,8 +28,8 @@ template<int dim=DIM>
 class Box {
   public:
     // simulation box
-    array<dim> lo;                     // lower bounds
-    array<dim> hi;                     // upper bounds
+    array<dim> lo;              // lower bounds
+    array<dim> hi;              // upper bounds
     Box(array<dim> lo, array<dim> hi) : lo(lo), hi(hi) {};
 };
 
