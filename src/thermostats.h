bf491e26929fd3026d3c231eed3e855dedf1ce28
chw90 <72135724+chw90@users.noreply.github.com>
2023-09-26 17:21:20 +0200

    Implement Gauss thermostat

--- src/thermostats.h
+++ src/thermostats.h
@@ -64,7 +64,38 @@ class ThermostatBehrendsen : public Thermostat<T, dim> {
         }
       }
     }
+};
 
+template<typename T=ParticleList<DIM>, int dim=DIM>
+class ThermostatGauss : public Thermostat<T, dim> {
+  // Gauss thermostat
+  public:
+    int const step;
+    ThermostatGauss(int step) : step(step) {};
+    void apply_forces(System<T, dim> &sys) {
+      // NOTE:
+      // Must be called *after* the particle forces are set according
+      // to the potential and *prior* to any other force modifier.
+
+      // compute zeta
+      double zeta_num = 0;      // numerator of zeta
+      double zeta_den = 0;      // denominator of zeta
+      for ( auto p: sys.particles ) {
+        for ( int i = 0; i < dim; i++ ) {
+          zeta_num += p.v[i]*p.f[i]; // assumes that p.f[i] = - dV/dx[i]
+          zeta_den += p.m*std::pow(p.v[i], 2);
+        }
+      }
+      auto zeta = - zeta_num/zeta_den;
+
+      // modify forces
+      for ( auto &p: sys.particles ) {
+        for ( int i = 0; i < dim; i++ ) {
+          p.f[i] += -zeta*p.m*p.v[i];
+        }
+      }
+    }
+    void apply_velocities(System<T, dim> &sys) {};
 };
 
 #endif // THERMOSTATS_H_
