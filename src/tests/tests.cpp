34dd9650bd37ac490d6f69a08bbb23cf2cb96e56
chw90 <72135724+chw90@users.noreply.github.com>
2023-12-17 18:06:36 +0100

    Add a unit test for the gravitational potential

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -1,130 +1,50 @@
-#include <iostream>
+#define BOOST_TEST_MODULE tests
+#include <boost/mpl/vector.hpp>
+#include <boost/test/unit_test.hpp>
 
-#include "containers.hpp"
 #include "particles.hpp"
-
-template <int dim = DIM>
-void print_particle(Particle<dim> p) {
-   std::cout << "m: " << p.m << " x: ";
-   for (auto &xi : p.x) {
-      std::cout << xi << " ";
-   }
-   std::cout << std::endl;
-}
-
-template <int dim = DIM>
-void print_pair(Particle<dim> pi, Particle<dim> pj) {
-   std::cout << "i: " << pi.m << " j " << pj.m << std::endl;
-}
-
-void test_ContainerVector() {
-   auto p1 = Particle<2>(1, 1.0, {0.0, 0.0}, {0.0, 0.0});
-   auto p2 = Particle<2>(2, 2.0, {0.0, 1.0}, {-1.0, 0.0});
-   auto p3 = Particle<2>(3, 3.0, {0.0, 5.36}, {-0.425, 0.0});
-
-   auto particles = ContainerVector<2>();
-   particles.insert(p1);
-   std::cout << particles.size() << std::endl;
-   particles.insert(p2);
-   std::cout << particles.size() << std::endl;
-   particles.insert(p3);
-   std::cout << particles.size() << std::endl;
-
-   std::cout << "map:" << std::endl;
-   particles.map(print_particle<2>);
-
-   std::cout << "map_pairwise:" << std::endl;
-   particles.map_pairwise(print_pair<2>);
-}
-
-void test_ContainerCells2D() {
-   auto p1 = Particle<2>(1, 1.0, {0.0, 0.0}, {0.0, 0.0});
-   auto p2 = Particle<2>(2, 2.0, {0.0, 0.3}, {-1.0, 0.0});
-   auto p3 = Particle<2>(3, 3.0, {0.3, 0.7}, {-0.425, 0.0});
-
-   std::array<double, 2> lo = {0.0, 0.0};
-   std::array<double, 2> hi = {1.0, 1.0};
-   auto b = Box<2>(lo, hi);
-
-   auto particles = ContainerCells<2>(b, 0.4, 1);
-   particles.insert(p1);
-   particles.insert(p2);
-   particles.insert(p3);
-
-   std::cout << "number of particles: " << particles.size() << std::endl;
-
-   particles.neighbor_build(b);
-
-   // move one particle to neighbor cell
-   auto ip = particles.data[0][0].begin();
-   (*ip).x[0] = 0.6;
-
-   // neighbor rebuild
-   particles.neighbor_build(b);
-
-   std::cout << "map:" << std::endl;
-   particles.map(print_particle<2>);
-
-   std::cout << "map_pairwise:" << std::endl;
-   particles.map_pairwise(print_pair<2>);
-
-   particles.map([](Particle<2> &p) { p.x[0] = 0.0; });
-}
+#include "potentials.hpp"
 
 template <int dim>
-void test_ContainerCells() {
-   int const N = 5;
-
-   double const lower = 0.0;
-   double const upper = 1.0;
-
-   std::array<double, dim> lo, hi;
-   lo.fill(lower);
-   hi.fill(upper);
-   auto b = Box<dim>(lo, hi);
-
-   // auto p0 = Particle<3>(0, 0.0, {0.7, 0.3, 0.3}, {0.0, 0.0, 0.0});
-   // auto p1 = Particle<3>(1, 1.0, {0.7, 0.3, 0.3}, {0.0, 0.0, 0.0});
-   // auto p2 = Particle<3>(2, 2.0, {0.7, 0.7, 0.7}, {0.0, 0.0, 0.0});
-   // auto p3 = Particle<3>(3, 3.0, {0.3, 0.7, 0.7}, {0.0, 0.0, 0.0});
-   // auto p4 = Particle<3>(4, 4.0, {0.7, 0.3, 0.7}, {0.0, 0.0, 0.0});
-
-   auto particles = ContainerCells<dim>(b, 0.4, 1);
-
-   std::uniform_real_distribution<double> x(lower, upper);
-   for (int i = 0; i < N; i++) {
-      auto pi = Particle<dim>(1, i);
-      for (int k = 0; k < dim; k++) {
-         pi.x[k] = x(RandomGenerator::engine);
+   requires(dim == 2 || dim == 3)
+struct Pair {
+   static const int dimension = dim;
+   Particle<dim> *p1, *p2;
+   Pair() {
+      if constexpr (dim == 2) {
+         p1 = new Particle<2>(1, 1.0, {0.0, 0.0}, {0.0, 0.0});
+         p2 = new Particle<2>(2, 2.0, {0.0, 1.0}, {-1.0, 0.0});
       }
-      particles.insert(pi);
+      if constexpr (dim == 3) {
+         p1 = new Particle<3>(1, 1.0, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0});
+         p2 = new Particle<3>(2, 2.0, {0.0, 0.0, 1.0}, {-1.0, 0.0, 0.0});
+      }
+   };
+   ~Pair() {
+      delete p1;
+      delete p2;
+   };
+};
+
+typedef boost::mpl::vector<Pair<2>, Pair<3>> test_potential_fixtures;
+
+BOOST_AUTO_TEST_SUITE(test_potential);
+
+BOOST_FIXTURE_TEST_CASE_TEMPLATE(test_potential_gravity, PairType, test_potential_fixtures, PairType) {
+   auto &p1 = *(PairType::p1);
+   auto &p2 = *(PairType::p2);
+   p1.f.fill(0);
+   p2.f.fill(0);
+
+   auto potential = PotentialGravitation<PairType::dimension>(1.0);
+   auto epot = potential.evaluate(p1, p2);
+   BOOST_TEST(epot == -2.0);
+   if constexpr (PairType::dimension == 2) {
+      std::vector<double> f1 = {0.0, 2.0};
+      std::vector<double> f2 = {0.0, -2.0};
+      BOOST_TEST(p1.f == f1, boost::test_tools::per_element());
+      BOOST_TEST(p2.f == f2, boost::test_tools::per_element());
    }
-
-   // particles.insert(p0);
-   // particles.insert(p1);
-   // particles.insert(p2);
-   // particles.insert(p3);
-   // particles.insert(p4);
-
-   particles.neighbor_build(b);
-
-   std::cout << "size: " << particles.data.num_elements() << std::endl;
-   std::cout << "shape: ";
-   for (size_t i = 0; i < particles.data.num_dimensions(); i++) {
-      std::cout << particles.data.shape()[i] << " ";
-   }
-   std::cout << std::endl;
-
-   std::cout << "map:" << std::endl;
-   particles.map(print_particle<dim>);
-
-   std::cout << "map_pairwise:" << std::endl;
-   particles.map_pairwise(print_pair<dim>);
 }
 
-int main() {
-   test_ContainerVector();
-   test_ContainerCells<2>();
-   test_ContainerCells<3>();
-   return 0;
-}
+BOOST_AUTO_TEST_SUITE_END();
